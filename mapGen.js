6ce5f58fe140f6a295370020db6c3f72f8c279c8
noahSerafin <noah.serafin04@gmail.com>
2023-06-28 01:19:09 +0100

    mapgenstart

---
+++ mapGen.js
@@ -0,0 +1,17 @@
+
+
+const drawPath = (inp, out, size) => {
+
+    let row = []
+    let arr = []
+    for (let i = 0; i < size; i++) {
+        row[i] = 0
+        arr[i] = row;
+    }
+    console.log(arr);
+
+    let position = inp;
+    let goal = out;
+}
+
+drawPath(1,1,8);
